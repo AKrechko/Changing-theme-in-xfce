22f73a770a9946fd0f52beb77d3e735eb0e406b9
Alexander <45324268+AKrechko@users.noreply.github.com>
2021-02-01 10:47:33 +0500

    Create theme_change.sh

---
+++ theme_change.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+get_theme=$(xfconf-query -c xsettings -p /Net/ThemeName)
+get_time=$(date +%k)
+
+if [ $get_time -gt 7 ] && [ $get_time -lt 18 ];
+then
+ if [ $get_theme == "Adwaita-dark" ] ;
+ then #Установим светлую тему с 8 до 17ч
+  xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita
+ else
+  exit
+ fi
+else # в другое время
+ if [ $get_theme == "Adwaita" ];
+ then #Установим темную тему 
+  xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita-dark
+ else 
+  exit
+ fi
+fi
