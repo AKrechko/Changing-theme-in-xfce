edd54ea1293f4ff95919adc590ad4ea3c08586ff
Alexander <45324268+AKrechko@users.noreply.github.com>
2021-02-01 13:33:04 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-get_theme=$(xfconf-query -c xsettings -p /Net/ThemeName)
-get_time=$(date +%k)
+while true; do
+        get_theme=$(xfconf-query -c xsettings -p /Net/ThemeName)
+        get_time=$(date +%k)
 
-if [ $get_time -gt 7 ] && [ $get_time -lt 18 ]; then
-	if [ $get_theme == "Adwaita-dark" ]; then #Установим светлую тему с 8 до 17ч
-		xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita
-	else
-		exit
-	fi
-else # в другое время
-	if [ $get_theme == "Adwaita" ]; then #Установим темную тему
-		xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita-dark
-	else
-		exit
-	fi
-fi
+        if [ $get_time -gt 7 ] && [ $get_time -lt 18 ]; then
+                if [ $get_theme == "Adwaita-dark" ]; then #Установим светлую тему с 8 до 17ч
+                        xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita
+                fi
+        else # в другое время
+                if [ $get_theme == "Adwaita" ]; then #Установим темную тему
+                        xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita-dark
+                fi
+        fi
+
+        sleep 60
+done
